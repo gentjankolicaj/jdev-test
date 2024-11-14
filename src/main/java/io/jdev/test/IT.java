56c4c596a4b9f18cfe7adbbde7c9207fb48899c0
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2024-11-14 15:53:57 +0100

    Added interfaces.

---
+++ src/main/java/io/jdev/test/IT.java
@@ -0,0 +1,11 @@
+package io.jdev.test;
+
+/**
+ * @author gentjan kolicaj
+ * @Date: 11/14/24 3:52 PM
+ * <p>
+ * Root interface of integration tests.
+ */
+public interface IT {
+
+}
---
+++ src/main/java/io/jdev/test/UT.java
@@ -0,0 +1,11 @@
+package io.jdev.test;
+
+/**
+ * @author gentjan kolicaj
+ * @Date: 11/14/24 3:53 PM
+ * <p>
+ * Root interface of unit tests
+ */
+public interface UT {
+
+}
